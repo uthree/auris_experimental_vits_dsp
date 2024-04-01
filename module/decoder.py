9bade6300a334f9c6014f46d195897d9e4e7c560
uthree <uthree271828@gmail.com>
2024-04-01 23:11:56 +0900

    write sourcenet, filternet

---
+++ module/decoder.py
@@ -0,0 +1,361 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.nn.utils import weight_norm, remove_weight_norm
+from .utils import get_padding, init_weights
+
+
+# Oscillate harmonic signal
+#
+# Inputs ---
+# f0: [BatchSize, 1, Frames]
+# uv: [BatchSize, 1, Frames], voiced = 1.0, unvocied = 0.0
+#
+# frame_size: int
+# num_harmonics: int
+# min_frequency: float
+# noise_scale: float
+#
+# Output: [BatchSize, NumHarmonics, Length]
+#
+# length = Frames * frame_size
+def oscillate_harmonics(f0,
+                        uv,
+                        frame_size=960,
+                        sample_rate=48000,
+                        num_harmonics=0):
+    N = f0.shape[0]
+    C = num_harmonics + 1
+    Lf = f0.shape[2]
+    Lw = Lf * frame_size
+
+    device = f0.device
+
+    # generate frequency of harmonics
+    mul = (torch.arange(C, device=device) + 1).unsqueeze(0).unsqueeze(2).expand(N, C, Lw)
+
+    # change length to wave's
+    fs = F.interpolate(f0, Lw, mode='linear') * mul
+
+    # unvoiced / voiced mask
+    uv = F.interpolate(uv, Lw, mode='linear')
+
+    # generate harmonics
+    I = torch.cumsum(fs / sample_rate, dim=2) # numerical integration
+    theta = 2 * math.pi * (I % 1) # convert to radians
+
+    harmonics = torch.sin(theta) * uv
+
+    return harmonics.to(device)
+
+
+# Oscillate aperiodic signal
+#
+# fft_bin = n_fft // 2 + 1
+# kernels: [BatchSize, fft_bin, Frames]
+#
+# Output: [BatchSize, 1, Frames * frame_size]
+def oscillate_aperiodic_signal(kernels, frame_size=960, n_fft=3840):
+    device = kernels.device
+    N = kernels.shape[0]
+    Lf = kernels.shape[2] # frame length
+    Lw = Lf * frame_size # waveform length
+    dtype = kernels.dtype()
+
+    gaussian_noise = torch.randn(N, Lw, device=device, dtype=torch.float)
+    kernels = kernels.to(torch.float) # to fp32
+
+    # calculate convolution in fourier-domain
+    # Since the input is an aperiodic signal such as Gaussian noise,
+    # there is no need to consider the phase on the kernel side.
+    w = torch.hann_window(n_fft, dtype=torch.float, device=device)
+    noise_stft = torch.stft(gaussian_noise, n_fft, frame_size, window=w)[:, :, 1:]
+    y_stft = noise_stft * kernels # In fourier domain, Multiplication means convolution.
+    y_stft = F.pad(y_stft, [1, 0]) # pad
+    y = torch.istft(y_stft, n_fft, frame_size, window=w)
+    y = y.unsqueeze(1)
+    y = y.to(dtype)
+    return y
+
+
+class FiLM(nn.Module):
+    def __init__(self, in_channels, condition_channels):
+        super().__init__()
+        self.to_shift = weight_norm(nn.Conv1d(condition_channels, in_channels, 1))
+        self.to_scale = weight_norm(nn.Conv1d(condition_channels, in_channels, 1))
+
+    def forward(self, x, c):
+        shift = self.to_shift(c)
+        scale = self.to_scale(c)
+        return x * scale + shift
+
+    def remove_weight_norm(self):
+        remove_weight_norm(self.to_shift)
+        remove_weight_norm(self.to_scale)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, channels, eps=1e-6):
+        super().__init__()
+        self.scale = nn.Parameter(torch.ones(1, channels, 1))
+        self.shift = nn.Parameter(torch.zeros(1, channels, 1))
+        self.eps = eps
+
+    def forward(self, x):
+        mu = x.mean(dim=(1, 2), keepdim=True)
+        sigma = x.std(dim=(1, 2), keepdim=True) + self.eps
+        x = (x - mu) / sigma
+        x = x * self.scale + self.shift
+        return x
+
+
+class GRN(nn.Module):
+    def __init__(self, channels, eps=1e-6):
+        super().__init__()
+        self.beta = nn.Parameter(torch.zeros(1, channels, 1))
+        self.gamma = nn.Parameter(torch.zeros(1, channels, 1))
+        self.eps = eps
+
+    def forward(self, x):
+        gx = torch.norm(x, p=2, dim=2, keepdim=True)
+        nx = gx / (gx.mean(dim=1, keepdim=True) + self.eps)
+        return self.gamma * (x * nx) + self.beta + x
+
+
+# ConvNeXt v2 + GeGLU
+class SourceNetLayer(nn.Module):
+    def __init__(self, channels=512, kernel_size=7, mlp_mul=3):
+        super().__init__()
+        padding = kernel_size // 2
+        self.c1 = nn.Conv1d(channels, channels, kernel_size, 1, padding, groups=channels)
+        self.norm = LayerNorm(channels)
+        self.c2 = nn.Conv1d(channels, channels * mlp_mul, 1)
+        self.c3 = nn.Conv1d(channels, channels * mlp_mul, 1)
+        self.grn = GRN(channels * mlp_mul)
+        self.c4 = nn.Conv1d(channels * mlp_mul, channels, 1)
+
+    def forward(self, x):
+        res = x
+        x = self.c1(x)
+        x = self.norm(x)
+        x = self.c2(x) * F.gelu(self.c3(x))
+        x = self.grn(x)
+        x = self.c4(x)
+        x = x + res
+        return x
+
+
+class SourceNet(nn.Module):
+    def __init__(self,
+                 content_channels=192,
+                 internal_channels=512,
+                 sample_rate=48000,
+                 frame_size=960,
+                 n_fft=3840,
+                 num_harmonics=30,
+                 kernel_size=7,
+                 num_layers=6,
+                 mlp_mul=3):
+        super().__init__()
+        self.input_layer = nn.Conv1d(content_channels, internal_channels)
+        self.input_norm = LayerNorm(internal_channels)
+        self.mid_layers = nn.Sequential(
+                *[SourceNetLayer(internal_channels, kernel_size, mlp_mul) for _ in range(num_layers)])
+        self.output_norm = LayerNorm(internal_channels)
+        self.to_amps = nn.Conv1d(internal_channels, num_harmonics + 1, 1)
+        self.to_kernels = nn.Conv1d(internal_channels, n_fft // 2 + 1, 1)
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = self.input_norm(x)
+        x = self.mid_layers(x)
+        x = output_norm(x)
+        amps = self.to_amps(x)
+        kernels = self.to_kernels(x)
+        return amps, kernels
+
+
+class ResBlock1(nn.Module):
+    def __init__(self, channels, condition_channels, kernel_size=3, dilations=[1, 3, 5]):
+        super().__init__()
+        self.convs1 = nn.ModuleList([])
+        self.convs2 = nn.ModuleList([])
+        self.films = nn.ModuleList([])
+
+        for d in dilations:
+            padding = get_padding(kernel_size, d)
+            self.convs1.append(
+                    weight_norm(
+                        nn.Conv1d(channels, channels, kernel_size, 1, padding, dilation=d, padding_mode='replicate')))
+            self.convs2.append(
+                    weight_norm(
+                        nn.Conv1d(channels, channels, kernel_size, 1, padding, 1, padding_mode='replicate')))
+            self.films.append(
+                    FiLM(channels, condition_channels))
+
+    def forward(self, x, c):
+        for c1, c2, film in zip(self.convs1, self.convs2, self.films):
+            res = x
+            x = F.leaky_relu(x, 0.1)
+            x = c1(x)
+            x = F.leaky_relu(x, 0.1)
+            x = c2(x)
+            x = film(x, c)
+            x = x + res
+        return x
+
+    def remove_weight_norm(self):
+        for c1, c2, film in zip(self.convs1, self.convs2, self.films):
+            remove_weight_norm(c1)
+            remove_weight_norm(c2)
+            film.remove_weight_norm()
+
+
+class MRF(nn.Module):
+    def __init__(self,
+                 channels,
+                 condition_channels,
+                 resblock_type='1',
+                 kernel_sizes=[3, 7, 11],
+                 dilations=[[1, 3, 5], [1, 3, 5], [1, 3, 5]]):
+        super().__init__()
+        self.blocks = nn.ModuleList([])
+        self.num_blocks = len(kernel_sizes)
+        if resblock_type == '1':
+            block = ResBlock1
+        for k, d in zip(kernel_sizes, dilations):
+            self.blocks.append(block(channels, condition_channels, k, d))
+
+    def forward(self, x, c):
+        xs = None
+        for block in self.blocks:
+            if xs is None:
+                xs = block(x, c)
+            else:
+                xs += block(x, c)
+        return xs / self.num_blocks
+
+    def remove_weight_norm(self):
+        for block in self.blocks:
+            block.remove_weight_norm()
+
+
+class UpBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 condition_channels,
+                 factor,
+                 resblock_type='1',
+                 kernel_sizes=[3, 7, 11],
+                 dilations=[[1, 3, 5], [1, 3, 5], [1, 3, 5]]):
+        super().__init__()
+        self.MRF = MRF(out_channels, condition_channels, resblock_type, kernel_sizes, dilations)
+        self.up_conv = weight_norm(
+                nn.ConvTranspose1d(in_channels, out_channels, factor*2, factor))
+        self.pad_left = factor // 2
+        self.pad_right = factor - pad_left
+
+    def forward(self, x, c):
+        x = F.leaky_relu(x, 0.1)
+        x = self.up_conv(x)
+        x = x[:, :, self.pad_left:-self.pad_right]
+        x = self.mrf(x, c)
+        return x
+
+    def remove_weight_norm(self):
+        remove_weight_norm(self.up_conv)
+        self.MRF.remove_weight_norm()
+
+
+class DownBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 factor,
+                 dilations=[[1, 2], [4, 8]],
+                 kernel_size=3):
+        super().__init__()
+        self.convs = nn.ModuleList([])
+        for ds in dilations:
+            cs = nn.ModuleList([])
+            for d in ds:
+                padding = get_padding(kernel_size, d)
+                cs.append(
+                        weight_norm(
+                            nn.Conv1d(in_channels, in_channels, kernel_size, 1, padding, dilation=d, padding_mode='replicate')))
+            self.convs.append(cs)
+        pad_left = factor // 2
+        pad_right factor - pad_left
+        self.pad = nn.ReplicationPad1d([pad_left, pad_right])
+        self.output_conv = weignt_norm(nn.Conv1d(in_channels, out_channels, factor*2, factor))
+
+    def forward(self, x):
+        for block in self.convs:
+            res = x
+            for c in block:
+                x = F.leaky_relu(x, 0.1)
+                x = c(x)
+            x = x + res
+        x = self.pad(x)
+        x = self.output_conv(x)
+        return x
+
+    def remove_weight_norm(self):
+        for block in self.convs:
+            for c in block:
+                remove_weight_norm(c)
+        remove_weight_norm(self.output_conv)
+
+
+class FilterNet(nn.Module):
+    def __init__(self,
+                 content_channels=192,
+                 channels=[512, 256, 128, 64, 32],
+                 factors=[5, 4, 4, 4, 3],
+                 num_harmonics=30,
+                 ):
+        super().__init__()
+        # input layer
+        self.content_in = weight_norm(nn.Conv1d(input_channels, channels[0], 1))
+
+        # downsamples
+        self.downs = nn.ModuleList([])
+        self.downs.append(weight_norm(nn.Conv1d(num_harmonics + 2, channels[-1], 1)))
+        cs = list(reversed(channels[1:]))
+        ns = cs[1:] + [channels[0]]
+        fs = list(reversed(factors[1:]))
+        for c, n, f, in zip(cs, ns, fs):
+            self.downs.append(DownBlock(c, n, f))
+
+        # upsamples
+        self.ups = nn.ModuleList([])
+        cs = channels
+        ns = channels[1:] + [channels[-1]]
+        fs = factors
+        for c, n, f in zip(cs, ns, fs):
+            self.ups.append(UpBlock(c, n, c, f))
+        self.output_layer = weight_norm(
+                nn.Conv1d(channels[-1], 1, 7, 1, 3, padding_mode='replicate'))
+
+    def forward(self, content, source):
+        x = self.content_in(content)
+
+        skips = []
+        for down in self.downs:
+            source = down(source)
+            skips.append(source)
+
+        for up, s in zip(self.ups, reversed(skips)):
+            x = up(x, s)
+        x = self.output_layer(x)
+        return x
+
+    def remove_weight_norm(self):
+        remove_weight_norm(self.content_in)
+        remove_weight_norm(self.output_layer)
+        for down in self.downs:
+            down.remove_weight_norm()
+        for up in self.ups:
+            up.remove_weight_norm()
---
+++ module/discriminator.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation)/2)
+
+
+class DiscriminatorP(nn.Module):
+    def __init__(self, period, kernel_size=5, stride=3, channels=32, num_layers=4, max_channels=256, use_spectral_norm=False):
+        super().__init__()
+        self.period = period
+        norm_f = nn.utils.weight_norm if use_spectral_norm == False else nn.utils.spectral_norm
+        
+        k = kernel_size
+        s = stride
+        c = channels
+
+        convs = [nn.Conv2d(1, c, (k, 1), (s, 1), (get_padding(5, 1), 0), padding_mode='replicate')]
+        for i in range(num_layers):
+            c_next = min(c * 2, max_channels)
+            convs.append(nn.Conv2d(c, c_next, (k, 1), (s, 1), (get_padding(5, 1), 0), padding_mode='replicate'))
+            c = c_next
+        self.convs = nn.ModuleList([norm_f(c) for c in convs])
+        self.post = norm_f(nn.Conv2d(c, 1, (3, 1), 1, (1, 0), padding_mode='replicate'))
+
+    def forward(self, x):
+        fmap = []
+
+        # 1d to 2d
+        b, c, t = x.shape
+        if t % self.period != 0:
+            n_pad = self.period - (t % self.period)
+            x = F.pad(x, (0, n_pad), "reflect")
+            t = t + n_pad
+        x = x.view(b, c, t // self.period, self.period)
+
+        for l in self.convs:
+            x = l(x)
+            x = F.leaky_relu(x, 0.1)
+            fmap.append(x)
+        x = self.post(x)
+        fmap.append(x)
+        return x, fmap
+
+
+class MultiPeriodicDiscriminator(nn.Module):
+    def __init__(self, periods, channels, max_channels, num_layers):
+        super().__init__()
+        self.sub_discs = nn.ModuleList([])
+        for p in periods:
+            self.sub_discs.append(DiscriminatorP(p,
+                                                 channels=channels,
+                                                 max_channels=max_channels,
+                                                 num_layers=num_layers))
+
+    def forward(self, x):
+        feats = []
+        logits = []
+        for d in self.sub_discs:
+            logit, fmap = d(x)
+            logits.append(logit)
+            feats += fmap
+        return logits, feats
+
+
+class DiscriminatorR(nn.Module):
+    def __init__(self, resolution=128, channels=16, num_layers=4):
+        super().__init__()
+        norm_f = nn.utils.weight_norm
+        self.convs = nn.ModuleList([norm_f(nn.Conv2d(1, channels, (7, 3), (2, 1), (3, 1)))])
+        self.hop_size = resolution
+        self.n_fft = resolution * 4
+        c = channels
+        for _ in range(num_layers):
+            self.convs.append(norm_f(nn.Conv2d(c, c*2, (5, 3), (2, 1), (2, 1))))
+            c = c * 2
+        self.post = norm_f(nn.Conv2d(c, 1, 3, 1, 1))
+
+    def forward(self, x):
+        x = x.sum(dim=1)
+        w = torch.hann_window(self.n_fft).to(x.device)
+        x = torch.stft(x, self.n_fft, self.hop_size, window=w, return_complex=True).abs()
+        x = x.unsqueeze(1)
+        feats = []
+        for l in self.convs:
+            x = l(x)
+            F.leaky_relu(x, 0.1)
+            feats.append(x)
+        x = self.post(x)
+        feats.append(x)
+        return x, feats
+
+
+class MultiResolutionDiscriminator(nn.Module):
+    def __init__(self, resolutions, channels, num_layers):
+        super().__init__()
+        self.sub_discs = nn.ModuleList([])
+        for r in resolutions:
+            self.sub_discs.append(DiscriminatorR(r, channels, num_layers))
+
+    def forward(self, x):
+        feats = []
+        logits = []
+        for d in self.sub_discs:
+            logit, fmap = d(x)
+            logits.append(logit)
+            feats += fmap
+        return logits, feats
+
+
+class Discriminator(nn.Module):
+    def __init__(self,
+                 resolutions=[128, 256, 512],
+                 periods=[1, 2, 3, 5, 7, 11, 17, 23, 31],
+                 mpd_num_layers=4,
+                 mrd_num_layers=4,
+                 mpd_channels=32,
+                 mrd_channels=32,
+                 mpd_max_channels=256,
+                 ):
+        super().__init__()
+        self.MPD = MultiPeriodicDiscriminator(periods, mpd_channels, mpd_max_channels, mpd_num_layers)
+        self.MRD = MultiResolutionDiscriminator(resolutions, mrd_channels, mrd_num_layers)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        mpd_logits, mpd_feats = self.MPD(x)
+        mrd_logits, mrd_feats = self.MRD(x)
+        return mpd_logits + mrd_logits, mpd_feats + mrd_feats
