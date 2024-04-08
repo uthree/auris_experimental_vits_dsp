8ccd9bb4b6c1e4357f92fdb695434588fc9a5e4e
uthree <uthree271828@gmail.com>
2024-04-08 18:54:09 +0900

    fix

--- test.py
+++ test.py
@@ -4,6 +4,5 @@ import torchaudio
 from module.infer import AurisInfer
 
 auris = AurisInfer("models/model.safetensors", "config/small.json")
-wf = auris.text_to_speech("こんにちは！", "jvs001", "ja")
-print(wf.shape)
+wf = auris.text_to_speech("こんにちは！、これはテストメッセージです。", "jvs003", "ja")
 torchaudio.save("output.wav", wf, sample_rate=48000)
