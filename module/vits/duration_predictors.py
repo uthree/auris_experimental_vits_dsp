5e87a732dd4cd62c8757ad5d504bd0314b587ee2
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-05-02 01:20:25 +0900

    Update duration_predictors.py
    Dialted -> Dilated

--- module/vits/duration_predictors.py
+++ module/vits/duration_predictors.py
@@ -153,7 +153,7 @@ class ConvFlow(nn.Module):
 
 class DDSConv(nn.Module):
     """
-    Dialted and Depth-Separable Convolution
+    Dilated and Depth-Separable Convolution
     """
 
     def __init__(self, channels, kernel_size, n_layers, p_dropout=0.0):
