3b0178e30f3d0d58c54a04dfe0fa001cfc9a2728
TylorShine <ricka.tylor@gmail.com>
2024-11-05 22:33:03 +0900

    Config: make subclass of dict

--- module/utils/config.py
+++ module/utils/config.py
@@ -1,36 +1,24 @@
 import json
 
 
-class Config:
+class Config(dict):
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
             if type(v) == dict:
                 v = Config(**v)
             self[k] = v
 
-    def keys(self):
-        return self.__dict__.keys()
-
-    def items(self):
-        return self.__dict__.items()
-
-    def values(self):
-        return self.__dict__.values()
-
     def __len__(self):
         return len(self.__dict__)
 
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __setitem__(self, key, value):
-        return setattr(self, key, value)
-
-    def __contains__(self, key):
-        return key in self.__dict__
-
-    def __repr__(self):
-        return self.__dict__.__repr__()
+    def __getattr__(*args):
+        val = dict.get(*args)
+        if isinstance(val, dict):
+            return Config(val)
+        return val
+    
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 
 def load_json_file(path):
